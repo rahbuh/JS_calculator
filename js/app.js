20e193cd39b0840c7aa9e061a7fe74ee4b78546d
Robert Kramer <robert.n.kramer@gmail.com>
2019-05-21 20:10:44 -0700

    added base JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log('connected')
+function getKeyInfo(e) {
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if (key) {
+    console.log(key);
+  }
+  const keys = document.querySelectorAll(".key");
+  //   console.log(keys);
+}
+
+// keys.forEach(key => {
+//   key.addEventListener("click", e => {
+//     key.classList.add("key-clicked");
+//   });
+//   keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+// });
+
+// window.addEventListener("keyup", e => {
+//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//   console.log(e.key);
+// });
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("key-clicked");
+}
+
+window.addEventListener("keyup", getKeyInfo);
+window.addEventListener("click", getKeyInfo);
