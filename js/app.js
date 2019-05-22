21aad267e5e78bb26801e3226e253836deb24ea1
Robert Kramer <robert.n.kramer@gmail.com>
2019-05-22 09:53:08 -0700

    changes to JS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,44 @@
-function keyAction(e) {
-  const key = document.querySelector(`.key[data-key="${e.key}"]`);
-  if (key) {
-    key.classList.add("key-clicked");
-    key.addEventListener("transitionend", removeTransition);
-  }
+// clear all values
+function init() {
+  const display = document.querySelector("#display");
+  display.textContent = "";
+  firstNum = 0;
+  secondNum = 0;
+  calcResult = 0;
+
+  holder = [];
 }
 
-function clickAction(e) {
-  e.target.classList.add("key-clicked");
-  e.target.addEventListener("transitionend", removeTransition);
+function adder(first) {
+  return function(second) {
+    return first + second;
+  };
+}
+
+function subtractor(first) {
+  return function(second) {
+    return first - second;
+  };
+}
+
+function multiplier(first) {
+  return function(second) {
+    return first * second;
+  };
+}
+
+function divider(first) {
+  return function(second) {
+    if (second === 0) {
+      return NaN;
+    }
+    return first / second;
+  };
+}
+
+function keyAnimation(key) {
+  key.classList.add("key-clicked");
+  key.addEventListener("transitionend", removeTransition);
 }
 
 function removeTransition(e) {
@@ -16,5 +46,62 @@ function removeTransition(e) {
   this.classList.remove("key-clicked");
 }
 
-window.addEventListener("keyup", keyAction);
-window.addEventListener("click", clickAction);
+function displayKey(key) {
+  const display = document.querySelector("#display");
+  display.textContent += key;
+}
+
+function keyAction(e) {
+  let firstNum = 0;
+  let secondNum = 0;
+  let calcResult = 0;
+  const holder = [];
+
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if (key) {
+    keyAnimation(key);
+    if (key.textContent === "C") {
+      const display = document.querySelector("#display");
+      display.textContent = "";
+      firstNum = 0;
+      secondNum = 0;
+      calcResult = 0;
+
+      holder = [];
+    }
+    if (key.textContent >= 0 || key.textContent <= 9) {
+      displayKey(key.textContent);
+      holder.push(key.textContent);
+    }
+    if (key.textContent === "." && holder.indexOf(".") === -1) {
+      displayKey(key.textContent);
+      holder.push(key.textContent);
+    }
+
+    if (key.textContent === "+") {
+      if (holder.length > 0) {
+        firstNum = Number(holder.join(""));
+        holder = [];
+        display.textContent = "";
+      }
+      const add = adder(firstNum);
+      console.log(firstNum);
+      console.log(holder);
+      console.log(add(3));
+    }
+
+  }
+    console.log(holder)
+}
+
+function clickAction(e) {
+  const key = e.target.textContent;
+
+  keyAnimation(e.target);
+}
+
+(function calculator() {
+  console.log("ready to calculate");
+  window.addEventListener("keyup", keyAction);
+  window.addEventListener("click", clickAction);
+})();
