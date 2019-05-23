30fab5dbf4bff5f1d9dae78beca7cedb70bb9b75
Robert Kramer <robert.n.kramer@gmail.com>
2019-05-22 21:54:08 -0700

    MVP

--- js/app.js
+++ js/app.js
@@ -25,51 +25,106 @@ function removeTransition(e) {
   this.classList.remove("key-clicked");
 }
 
-// Show select number or decimal point on display
+// Show selected number or decimal point on display
 function displayKey(key, display) {
   display.textContent += key;
 }
 
-// Operation closure functions
+// Operator closure functions
 function makeAdd(x) {
   return function(y) {
     return x + y;
   };
 }
 
-function makeSubtract(x) {
+function makeSub(x) {
   return function(y) {
     return x - y;
   };
 }
 
-function makeMultiply(x) {
+function makeMul(x) {
   return function(y) {
     return x * y;
   };
 }
 
-function makeDivide(x) {
+function makeDiv(x) {
   return function(y) {
-    return x * y;
+    if (y === 0) {
+      return "Error: cannot divide by 0";
+    }
+    return x / y;
   };
 }
 
-function createOperator(data, makeOp) {
-  let currentNum = Number(data.holder.join("")) + data.calcResult;
-  data.holder.length = 0;
-  data.holder.push(0);
-  data.display.textContent = currentNum;
-  return makeOp(currentNum);
+// Create operator closure
+function createOperator(num, makeOp) {
+  return makeOp(num);
 }
 
-function performCalc(data) {
-  let currentNum = Number(data.holder.join(""));
+// Use operator closure to calculate
+function performCalc(num) {
+  return data.operator(num);
+}
+
+function resetHolder(data) {
   data.holder.length = 0;
   data.holder.push(0);
-  let result = data.operator(currentNum);
-  data.display.textContent = result;
-  return result;
+}
+
+function performOperation(fn) {
+  if (!data.operator) {
+    data.calcResult += Number(data.holder.join(""));
+    data.operator = createOperator(data.calcResult, fn);
+  } else {
+    data.calcResult = performCalc(Number(data.holder.join("")));
+    data.operator = createOperator(data.calcResult, fn);
+  }
+  data.display.textContent = data.calcResult;
+  resetHolder(data);
+}
+
+function calculation(key) {
+  if (key.textContent === "C") {
+    init(data);
+  } // clear (reset)
+
+  if (key.textContent >= 0 || key.textContent <= 9) {
+    if (data.holder.length <= 1) {
+      data.display.textContent = "";
+    } // removes leading zero for numbers only
+
+    displayKey(key.textContent, data.display);
+    data.holder.push(key.textContent);
+  }
+  if (key.textContent === "." && data.holder.indexOf(".") === -1) {
+    displayKey(key.textContent, data.display);
+    data.holder.push(key.textContent);
+  } // only allow decimal point one time
+
+  if (key.textContent === "+") {
+    performOperation(makeAdd);
+  }
+
+  if (key.textContent === "-") {
+    performOperation(makeSub);
+  }
+
+  if (key.textContent === "*") {
+    performOperation(makeMul);
+  }
+
+  if (key.textContent === "/") {
+    performOperation(makeDiv);
+  }
+
+  if (key.textContent === "=") {
+    data.calcResult = performCalc(Number(data.holder.join("")));
+    data.operator = null;
+    data.display.textContent = data.calcResult;
+    resetHolder(data);
+  }
 }
 
 // if key pressed on keyboard
@@ -77,51 +132,19 @@ function keyAction(e) {
   const key = document.querySelector(`.key[data-key="${e.key}"]`);
   if (key) {
     keyAnimation(key);
-
-    if (key.textContent === "C") {
-      init(data);
-    }
-
-    if (key.textContent >= 0 || key.textContent <= 9) {
-      if (data.holder.length <= 1) {
-        data.display.textContent = "";
-        displayKey(key.textContent, data.display);
-        data.holder.push(key.textContent);
-      } else {
-        displayKey(key.textContent, data.display);
-        data.holder.push(key.textContent);
-      }
-    }
-
-    if (key.textContent === "." && data.holder.indexOf(".") === -1) {
-      displayKey(key.textContent, data.display);
-      data.holder.push(key.textContent);
-    }
-
-    if (key.textContent === "+") {
-      if (!data.operator) {
-        console.log("createOperator");
-        data.operator = createOperator(data, makeAdd);
-      } else {
-        console.log("performCalc");
-        data.calcResult = performCalc(data);
-        data.operator = null;
-      }
-    }
-
-    console.log("holder: ", data.holder);
+    calculation(key);
   }
 }
 
 // if key clicked in browser
 function clickAction(e) {
-  const key = e.target.textContent;
-  keyAnimation(e.target);
+  // e.preventDefault;
+  // const key = e.target.textContent;
+  // keyAnimation(e.target);
 }
 
 (function calculator() {
-  console.log("ready to calculate");
   data.display.textContent = "0";
-  window.addEventListener("keyup", keyAction);
-  window.addEventListener("click", clickAction);
+  document.addEventListener("keyup", keyAction);
+  document.addEventListener("click", clickAction);
 })();
