ba65a5ef28c731dd1bd6e1e54430d3ed1f5e7651
Robert Kramer <robert.n.kramer@gmail.com>
2019-05-22 17:48:22 -0700

    adder functionality complete

--- js/app.js
+++ js/app.js
@@ -1,41 +1,20 @@
+const data = {
+  display: document.querySelector("#display"),
+  calcResult: 0,
+  operator: null,
+  holder: [0]
+};
+
 // clear all values
-function init() {
-  const display = document.querySelector("#display");
-  display.textContent = "";
-  firstNum = 0;
-  secondNum = 0;
-  calcResult = 0;
-
-  holder = [];
+function init(data) {
+  data.display.textContent = "0";
+  data.calcResult = 0;
+  data.operator = null;
+  data.holder.length = 0;
+  data.holder.push(0);
 }
 
-function adder(first) {
-  return function(second) {
-    return first + second;
-  };
-}
-
-function subtractor(first) {
-  return function(second) {
-    return first - second;
-  };
-}
-
-function multiplier(first) {
-  return function(second) {
-    return first * second;
-  };
-}
-
-function divider(first) {
-  return function(second) {
-    if (second === 0) {
-      return NaN;
-    }
-    return first / second;
-  };
-}
-
+// Show key pressed animation
 function keyAnimation(key) {
   key.classList.add("key-clicked");
   key.addEventListener("transitionend", removeTransition);
@@ -46,62 +25,103 @@ function removeTransition(e) {
   this.classList.remove("key-clicked");
 }
 
-function displayKey(key) {
-  const display = document.querySelector("#display");
+// Show select number or decimal point on display
+function displayKey(key, display) {
   display.textContent += key;
 }
 
-function keyAction(e) {
-  let firstNum = 0;
-  let secondNum = 0;
-  let calcResult = 0;
-  const holder = [];
+// Operation closure functions
+function makeAdd(x) {
+  return function(y) {
+    return x + y;
+  };
+}
 
+function makeSubtract(x) {
+  return function(y) {
+    return x - y;
+  };
+}
+
+function makeMultiply(x) {
+  return function(y) {
+    return x * y;
+  };
+}
+
+function makeDivide(x) {
+  return function(y) {
+    return x * y;
+  };
+}
+
+function createOperator(data, makeOp) {
+  let currentNum = Number(data.holder.join("")) + data.calcResult;
+  data.holder.length = 0;
+  data.holder.push(0);
+  data.display.textContent = currentNum;
+  return makeOp(currentNum);
+}
+
+function performCalc(data) {
+  let currentNum = Number(data.holder.join(""));
+  data.holder.length = 0;
+  data.holder.push(0);
+  let result = data.operator(currentNum);
+  data.display.textContent = result;
+  return result;
+}
+
+// if key pressed on keyboard
+function keyAction(e) {
   const key = document.querySelector(`.key[data-key="${e.key}"]`);
   if (key) {
     keyAnimation(key);
-    if (key.textContent === "C") {
-      const display = document.querySelector("#display");
-      display.textContent = "";
-      firstNum = 0;
-      secondNum = 0;
-      calcResult = 0;
 
-      holder = [];
+    if (key.textContent === "C") {
+      init(data);
     }
+
     if (key.textContent >= 0 || key.textContent <= 9) {
-      displayKey(key.textContent);
-      holder.push(key.textContent);
+      if (data.holder.length <= 1) {
+        data.display.textContent = "";
+        displayKey(key.textContent, data.display);
+        data.holder.push(key.textContent);
+      } else {
+        displayKey(key.textContent, data.display);
+        data.holder.push(key.textContent);
+      }
     }
-    if (key.textContent === "." && holder.indexOf(".") === -1) {
-      displayKey(key.textContent);
-      holder.push(key.textContent);
+
+    if (key.textContent === "." && data.holder.indexOf(".") === -1) {
+      displayKey(key.textContent, data.display);
+      data.holder.push(key.textContent);
     }
 
     if (key.textContent === "+") {
-      if (holder.length > 0) {
-        firstNum = Number(holder.join(""));
-        holder = [];
-        display.textContent = "";
+      if (!data.operator) {
+        console.log("createOperator");
+        data.operator = createOperator(data, makeAdd);
+      } else {
+        console.log("performCalc");
+        data.calcResult = performCalc(data);
+        data.operator = null;
       }
-      const add = adder(firstNum);
-      console.log(firstNum);
-      console.log(holder);
-      console.log(add(3));
     }
 
+    console.log("holder: ", data.holder);
   }
-    console.log(holder)
 }
 
+// if key clicked in browser
 function clickAction(e) {
   const key = e.target.textContent;
-
   keyAnimation(e.target);
 }
 
 (function calculator() {
   console.log("ready to calculate");
+  data.display.textContent = "0";
   window.addEventListener("keyup", keyAction);
   window.addEventListener("click", clickAction);
 })();
